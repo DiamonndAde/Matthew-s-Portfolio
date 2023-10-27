321bcf5214b877300b9764abee1bbf8ddcf9e0c6
matthew <ayindematthew2003@gmail.com>
2023-10-28 00:13:04 +0100

    logo change and andriod config

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -53,7 +53,7 @@ const ComputersCanvas = () => {
       frameLoop='demand'
       shadows
       camera={{position: [20, 3, 5], fov: 25}}
-      gl={{preserveDrawingBuffer: true}}>
+      gl={{preserveDrawingBuffer: true, alpha: true}}>
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls 
             enableZoom={false}
